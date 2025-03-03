29d23ca6a9a9c77383b1edb97c47c795ded24277
Matheus Galvao <matheusrigonigalvai@gmail.com>
2025-03-03 15:20:42 +0100

    Handling other 400 errors using JSON format

--- app/routes/errors.py
+++ app/routes/errors.py
@@ -9,6 +9,13 @@ def handle_not_found_error(error):
         "endpoint": request.path
     }), 404
 
+@errors_bp.app_errorhandler(400)
+def handle_bad_request_error(error):
+    return jsonify({
+        "error": str(error),
+        "endpoint": request.path
+    }), 400
+
 @errors_bp.app_errorhandler(500)
 def handle_internal_server_error(error):
     return jsonify({
